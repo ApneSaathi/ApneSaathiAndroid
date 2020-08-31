0fd8b8d3c5c343686a7aa54e5c533d0710f05c9a
sunil <sunilsunny565@gmail.com>
2020-08-31 13:20:19 +0530

    Comments added

--- app/src/main/java/com/nitiaayog/apnesaathi/adapter/BaseArrayAdapter.kt
+++ app/src/main/java/com/nitiaayog/apnesaathi/adapter/BaseArrayAdapter.kt
@@ -4,9 +4,17 @@ import android.content.Context
 import android.widget.ArrayAdapter
 import android.widget.Filter
 
+/**
+ *  An adapter for filtering the values
+ * [ArrayAdapter] is the default adapter from android library.
+ */
 class BaseArrayAdapter(context: Context, resource: Int, objects: Array<String?>) :
     ArrayAdapter<String?>(context, resource, objects) {
 
+    /**
+     * Method for filtering the values.
+     *  [Filter] is a default class from the android library
+     */
     override fun getFilter(): Filter {
         return object : Filter() {
             override fun performFiltering(constraint: CharSequence?): FilterResults? = null
